ee30f692cc312b4420e3e0209f98498fd332e19e
vagon14.0255@gmail.com <vagon14.0255@gmail.com>
2024-03-03 13:41:50 +0300

    www

--- telegram.js
+++ telegram.js
@@ -124,7 +124,7 @@ async function processUrlsAndWriteToExcel(urls) {
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
 
   try {
-
+    await page.waitForFunction('document.querySelector(".categories__category-item_title") !== null');
 
   let categoriesTitles = [];
 
@@ -133,8 +133,7 @@ async function processUrlsAndWriteToExcel(urls) {
     try {
       await page.goto(link, { waitUntil: 'domcontentloaded', timeout: 60000 });
       await page.waitForFunction('document.querySelector(".categories__category-item_title") !== null');
-
-
+      console.log(page.url());
       await page.waitForSelector('.categories__category-item_title' ,{timeout: 130000 }); 
   
 
