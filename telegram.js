0d23bc44555c0db2766bb32c5c0a9286a9bc66a2
vagon14.0255@gmail.com <vagon14.0255@gmail.com>
2024-03-03 13:51:14 +0300

    www

--- telegram.js
+++ telegram.js
@@ -124,7 +124,7 @@ async function processUrlsAndWriteToExcel(urls) {
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
 
   try {
-    await page.waitForFunction('document.querySelector(".categories__category-item_title") !== null');
+    await page.waitForFunction('document.querySelector(".categories__category-item_title") !== null', {timeout: 180000});
 
   let categoriesTitles = [];
 
