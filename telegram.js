a627bbe531efabb6655ba6442444259e936324db
vagon14.0255@gmail.com <vagon14.0255@gmail.com>
2024-03-08 01:36:59 +0300

    7s

--- telegram.js
+++ telegram.js
@@ -1,9 +1,11 @@
 const ExcelJS = require('exceljs');
 const puppeteer = require('puppeteer');
 const express = require('express');
+const chalk = require('chalk');
 require('dotenv').config()
 const app = express();
 const port = 3000;
+
 
 app.use(express.json());
 
@@ -14,43 +16,81 @@ app.use(express.static('public'));
 const TelegramBot = require('node-telegram-bot-api');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const bot = new TelegramBot(process.env.TOKEN , { polling: true });
+const userStates = {}; 
+const allowedUserIds = [1190709922,6358500758];
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
+bot.on('message', async (msg) => { 
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const userId = msg.from.id;
 
 
-    const urls = text.split(/\s+/).filter(url => url.startsWith('http://') || url.startsWith('https://'));;
+  if (!allowedUserIds.includes(userId)) {
+    await bot.sendMessage(chatId, 'У вас нет доступа к использованию этого бота.');
+    return; 
+  }
+  
+  if (!msg.text) {
+    await bot.sendMessage(chatId, 'Пожалуйста, отправьте текстовое сообщение.');
+    return; 
+  }
 
-    if(urls.length > 0) {
-
-        fetch('http://localhost:3000/process-urls', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ urls }),
-        })
-        .then(response => response.json())
-        .then(data => {
-            bot.sendMessage(chatId, 'URL успешно обработаны.');
-        })
-        .catch(error => {
-            console.error('Ошибка:', error);
-            bot.sendMessage(chatId, 'Произошла ошибка при обработке URL.');
-        });
-    } else {
-        bot.sendMessage(chatId, 'Пожалуйста, отправьте корректный URL.');
+  if(userStates[chatId] && userStates[chatId].awaitingPrice) {
+     
+    const price = parseFloat(text);
+    console.log(`Получена цена: ${text}`); 
+    if (isNaN(price)) {
+      await bot.sendMessage(chatId, 'Пожалуйста, введите валидную цену.');
+      return;
     }
-});
 
+
+    userStates[chatId].price = price;
+    userStates[chatId].awaitingPrice = false;
+    await processUrlsAndWriteToExcel([userStates[chatId].url], price, chatId); 
+    await bot.sendMessage(chatId, '🔰Данные успешно записаны📝');
+    await bot.sendMessage(chatId, '📱Можете отправлять следующий URL📲');
+    delete userStates[chatId];
+    return;
+
+  } else {
+    const urls = text.split(/\s+/).filter(url => url.startsWith('http://') || url.startsWith('https://'));
+    if(urls.length > 0) {
+      userStates[chatId] = { url: urls[0], awaitingPrice: true };
+      const response = await fetch('http://localhost:3000/process-urls', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ urls }),
+    });
+    let sentMessage = await bot.sendMessage(chatId, 'Подготовка к обработке URL...');
+    let messageId = sentMessage.message_id;
+
+    await new Promise(resolve => setTimeout(resolve, 1000)); 
+    await bot.editMessageText('██████████████', { chat_id: chatId, message_id: messageId });
+    await new Promise(resolve => setTimeout(resolve, 1000)); 
+    await bot.editMessageText('████████████████████', { chat_id: chatId, message_id: messageId });
+    await new Promise(resolve => setTimeout(resolve, 1000)); 
+    await bot.editMessageText('███████████████████████████', { chat_id: chatId, message_id: messageId });
+    await new Promise(resolve => setTimeout(resolve, 1000)); 
+    await bot.editMessageText('🔰Готово🔰', { chat_id: chatId, message_id: messageId });
+    await new Promise(resolve => setTimeout(resolve, 900)); 
+    await bot.editMessageText('📁Данные записаны.[100%]', { chat_id: chatId, message_id: messageId });
+
+      
+      userStates[chatId] = { url: urls[0], awaitingPrice: true };
+      await bot.sendMessage(chatId, 'Цена товара💸:');
+    } else {
+      await bot.sendMessage(chatId, 'Пожалуйста, отправьте корректный URL.');
+    }
+  }
+});
 
 app.post('/process-urls', async (req, res) => {
     const { urls } = req.body; 
     console.log(urls); 
-
     try {
-        await processUrlsAndWriteToExcel(urls); 
         res.json({message: 'Данные успешно обработаны'});
     } catch (error) {
         console.error('Ошибка при обработке URL и записи в Excel:', error);
@@ -60,7 +100,7 @@ app.post('/process-urls', async (req, res) => {
 
 // Запуск сервера
 app.listen(port, () => {
-    console.log(`Сервер запущен на http://localhost:${port}`);
+    console.log(chalk.underline.bold.bgGreenBright.blue(`Сервер запущен на http://localhost:${port}`));
 });
 
 
@@ -69,9 +109,6 @@ async function writeToExcel(data, filePath) {
   try {
     await workbook.xlsx.readFile(filePath);
     let worksheet = workbook.getWorksheet('Список товаров');
-  
-
-
     
     if (!worksheet) {
       console.error('Лист "Список товаров" не найден');
@@ -80,59 +117,55 @@ async function writeToExcel(data, filePath) {
 
     let rowNumber = worksheet.lastRow ? worksheet.lastRow.number + 1 : 3;
 
-    console.log('Начинаем запись данных в Excel');
-    data.forEach(({ categoryTitle, productTitle, brand, manufacturerArticle, price }) => {
+    console.log(chalk.underline.bold.bgGreen.black('Начинаем запись данных в Excel'));
+    data.forEach(({ categoryTitle, productTitle, brand, manufacturerArticle, price , model }) => {
+      const productId = `krep_${rowNumber - 2}`;
+
+      worksheet.getCell(`A${rowNumber}`).value = productId;
       worksheet.getCell(`B${rowNumber}`).value = "Доступен";
       worksheet.getCell(`C${rowNumber}`).value = categoryTitle;
       worksheet.getCell(`G${rowNumber}`).value = productTitle;
       worksheet.getCell(`H${rowNumber}`).value = price; 
       worksheet.getCell(`D${rowNumber}`).value = brand;
       worksheet.getCell(`E${rowNumber}`).value = manufacturerArticle;
+      worksheet.getCell(`F${rowNumber}`).value = model; 
       worksheet.getCell(`J${rowNumber}`).value = "100";
       worksheet.getCell(`P${rowNumber}`).value = "17";
-      worksheet.getCell(`Q${rowNumber}`).value = "4дня";
+      worksheet.getCell(`Q${rowNumber}`).value = "4 дня";
+      worksheet.getCell(`K${rowNumber}`).value = "Не облагается";
       rowNumber++;
   });
-    
 
     await workbook.xlsx.writeFile(filePath);
-    console.log('Данные записаны в файл:', filePath);
+    console.log(chalk.underline.bold.bgRgb(222, 78, 29).black('Данные записаны в файл:', filePath));
 
 
   } catch (error) {
-    console.error('Произошла ошибка при работе с Excel:', error);
+    console.error(chalk.underline.bold.bgRed.black('Произошла ошибка при работе с Excel:', error));
   }
 }
 
-async function processUrlsAndWriteToExcel(urls) {
-
+async function processUrlsAndWriteToExcel(urls, price) {
+  let categoriesTitles = [];
   let browser;
   try {
     const browser = await puppeteer.launch({    
-       headless: false
+       headless: true
   });
-
 
   const page = await browser.newPage();
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
 
   try {
-
-
-  let categoriesTitles = [];
-
   for (let link of urls) {
     let categoryTitle = ''; 
     let productTitle = '';
-    let price = 'Цена не найдена'; 
+
     const navigationPromise = page.waitForNavigation({waitUntil: 'networkidle0'});
     try {
       await page.goto(link, { waitUntil: 'domcontentloaded', timeout: 60000 });
       await navigationPromise;
 
-
-     
-  
 
       try {  
         await page.waitForSelector('.categories__category-item_title', { timeout: 5000 }); 
@@ -158,22 +191,11 @@ async function processUrlsAndWriteToExcel(urls) {
         console.error('Название продукта не найдено для', link);
         productTitle = 'Название продукта не найдено'; 
       }
-      await navigationPromise;
-      try {
-        let priceText = await page.evaluate(() => {
-          const priceElement = document.querySelector('.sales-block-offer-price__price-final');
-          return priceElement ? priceElement.textContent.trim() : 'Цена не найдена';
-        });
-        price = priceText.match(/\d+/g)?.join('') || 'Цена не найдена'; 
-      } catch {
-        console.error('Цена не найдена для', link);
-
-      }
 
       await navigationPromise;
-      const { brand, manufacturerArticle } = await page.evaluate(() => {
+      const { brand, manufacturerArticle, model } = await page.evaluate(() => {
         const items = Array.from(document.querySelectorAll('.pdp-specs__item'));
-        const data = { brand: '', manufacturerArticle: '' };
+        const data = { brand: '', manufacturerArticle: '', model: '' };
   
         items.forEach((item) => {
           const nameElement = item.querySelector('.pdp-specs__item-name');
@@ -187,14 +209,16 @@ async function processUrlsAndWriteToExcel(urls) {
             data.brand = value;
           } else if (name === "Артикул производителя") {
             data.manufacturerArticle = value;
+          } else if (name === "Модель") { 
+            data.model = value;
           }
         });
-  
+      
         return data;
       });
       await navigationPromise;
 
-      categoriesTitles.push({ categoryTitle, productTitle, brand, manufacturerArticle, price });
+      categoriesTitles.push({ categoryTitle, productTitle, brand, manufacturerArticle , model , price });
     } catch (error) {
       console.error(`Произошла ошибка при обработке ${link}:`, error);
     }
