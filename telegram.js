cb107a9860925844a55c9d5f70b85d16e56e2840
vagon14.0255@gmail.com <vagon14.0255@gmail.com>
2024-03-08 11:09:30 +0300

    8s

--- telegram.js
+++ telegram.js
@@ -154,7 +154,7 @@ async function processUrlsAndWriteToExcel(urls, price) {
   });
 
   const page = await browser.newPage();
-  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36');
 
   try {
   for (let link of urls) {
